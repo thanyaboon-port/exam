00d237eea74ce78cf1302bd5cd482d08d566d6c5
thanyaboon-port <111132724+thanyaboon-port@users.noreply.github.com>
2022-08-13 15:52:42 +0700

    Update shell_setup_hdfs.sh
    update

--- shell_script/shell_setup_hdfs.sh
+++ shell_script/shell_setup_hdfs.sh
@@ -2,14 +2,20 @@
 
 set -e  # stop immediately on any error
 
-
+#hdfs dfs -mkdir /data
 for dir in source datalake datawarehouse
 do
-    hadoop fs mkdir /user/hdfs/$dir
+    echo "create path /data/$dir"
+    hdfs dfs -mkdir /data/$dir
 done
 
 for dir in order_detail restaurant_detail
 do
-    hadoop fs mkdir /user/hdfs/$dir
+    echo "create path /data/source/$dir"
+    hdfs dfs -mkdir /data/source/$dir
+    echo "create path /data/datalake/$dir"
+    hdfs dfs -mkdir /data/datalake/$dir
+    echo "create path /data/datawarehouse/$dir"
+    hdfs dfs -mkdir /data/datawarehouse/$dir
 done
 
