7900819e03db264e359a08415a91ead1b2e0f2cd
thanyaboon hirumdomrong <thanyaboonhirumdomrong@thanyaboons-MacBook-Pro.local>
2022-08-14 10:12:24 +0700

    update code

--- shell_script/create_postgres_table.sh
+++ shell_script/create_postgres_table.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 set -e  # stop immediately on any error
+path='/exam/postgres_create_table'
+
 psql -c 'create database linemanwongnai;'
+
 for script in create_table_order_detail.sql create_table_restaurant_detail.sql
 do
   echo "processing $script"
-  select=`cat $script`
+  select=`cat $path/$script`
   psql -d linemanwongnai -U postgres <<EOF
-DROP TABLE IF EXISTS "$script";
 $select
 EOF
 done
@@ -16,7 +18,7 @@ echo "start ingest data to order_detail"
 THE_USER=postgres
 THE_DB=linemanwongnai
 THE_TABLE=order_detail
-THE_DIR=/var/lib/postgresql/data
+THE_DIR=/exam/data
 THE_FILE=order_detail.csv
 
 psql -c "\copy ${THE_TABLE} FROM '${THE_DIR}/${THE_FILE}' delimiter ',' csv"
@@ -25,7 +27,7 @@ echo "start ingest data to restaurant_detail"
 THE_USER=postgres
 THE_DB=linemanwongnai
 THE_TABLE=restaurant_detail
-THE_DIR=/var/lib/postgresql/data
+THE_DIR=/exam/data
 THE_FILE=restaurant_detail.csv
 
 psql -c "\copy ${THE_TABLE} FROM '${THE_DIR}/${THE_FILE}' delimiter ',' csv"
